720b095ad545770f3860263ba738a32102d9b16a
Saravanan Mani <saravananmani@bitgo.com>
2023-03-13 22:24:49 +0530

    feat(utxo-lib): code for psbt musig2 test is refactored
    To make it smaller separate files for incoming test suites
    
    TICKET: BG-66933

--- modules/utxo-lib/test/bitgo/psbt/Musig2Psbt.ts
+++ modules/utxo-lib/test/bitgo/psbt/Musig2Psbt.ts
@@ -1,179 +1,28 @@
 import * as assert from 'assert';
 
-import { networks } from '../../../src';
-import {
-  isWalletUnspent,
-  Unspent,
-  getInternalChainCode,
-  getExternalChainCode,
-  outputScripts,
-  unspentSum,
-  UtxoTransaction,
-  createPsbtForNetwork,
-  addWalletUnspentToPsbt,
-  addWalletOutputToPsbt,
-  KeyName,
-  UtxoPsbt,
-  PSBT_PROPRIETARY_IDENTIFIER,
-  scriptTypeForChain,
-  WalletUnspent,
-  ProprietaryKeySubtype,
-} from '../../../src/bitgo';
+import { getInternalChainCode, PSBT_PROPRIETARY_IDENTIFIER, scriptTypeForChain } from '../../../src/bitgo';
 
 import { getDefaultWalletKeys } from '../../../src/testutil';
-import { mockWalletUnspent } from '../../../src/testutil/mock';
 import {
   createTapInternalKey,
   createTapOutputKey,
   decodePsbtMusig2ParticipantsKeyValData,
   setMusig2Nonces,
 } from '../../../src/bitgo/Musig2';
+import { scriptTypes2Of3 } from '../../../src/bitgo/outputScripts';
 import {
-  createKeyPathP2trMusig2,
-  createPaymentP2trMusig2,
-  ScriptType2Of3,
-  scriptTypes2Of3,
-  toXOnlyPublicKey,
-} from '../../../src/bitgo/outputScripts';
+  constructPsbt,
+  getUnspents,
+  validateNoncesKeyVals,
+  validateParticipantsKeyVals,
+  validatePsbtP2trMusig2Input,
+  validatePsbtP2trMusig2Output,
+} from './PsbtMusig2Util';
 
 describe('p2trMusig2 PSBT Test Suite', function () {
-  const network = networks.bitcoin;
   const rootWalletKeys = getDefaultWalletKeys();
   const outputType = 'p2trMusig2';
   const CHANGE_INDEX = 100;
-  const FEE = BigInt(100);
-
-  function constructPsbt(
-    unspents: (Unspent<bigint> & { prevTx?: Buffer })[],
-    signer: KeyName,
-    cosigner: KeyName,
-    outputType: outputScripts.ScriptType2Of3
-  ): UtxoPsbt<UtxoTransaction<bigint>> {
-    const psbt = createPsbtForNetwork({ network });
-    const total = BigInt(unspentSum<bigint>(unspents, 'bigint'));
-    addWalletOutputToPsbt(psbt, rootWalletKeys, getInternalChainCode(outputType), CHANGE_INDEX, total - FEE);
-    unspents.forEach((u) => {
-      if (isWalletUnspent(u)) {
-        addWalletUnspentToPsbt(psbt, u, rootWalletKeys, signer, cosigner);
-      } else {
-        throw new Error(`invalid unspent`);
-      }
-    });
-    return psbt;
-  }
-
-  function getUnspents(inputScriptTypes: ScriptType2Of3[]) {
-    return inputScriptTypes.map((t, i): WalletUnspent<bigint> => {
-      if (!outputScripts.isScriptType2Of3(t)) {
-        throw new Error(`invalid input type ${t}`);
-      }
-      const unspent = mockWalletUnspent(network, BigInt('10000000000000000'), {
-        keys: rootWalletKeys,
-        chain: getExternalChainCode(t),
-        vout: i,
-      });
-
-      if (isWalletUnspent(unspent)) {
-        return unspent;
-      }
-      throw new Error('Invalid unspent');
-    });
-  }
-
-  function validatePsbtP2trMusig2Input(
-    psbt: UtxoPsbt<UtxoTransaction<bigint>>,
-    index: number,
-    unspent: WalletUnspent<bigint>,
-    spendType: 'keyPath' | 'scriptPath'
-  ) {
-    const input = psbt.data.inputs[index];
-    assert.strictEqual(input.tapBip32Derivation?.length, 2);
-    let leafHashesCount = 0;
-
-    if (spendType === 'keyPath') {
-      const inputWalletKeys = rootWalletKeys.deriveForChainAndIndex(unspent.chain, unspent.index);
-      const { internalPubkey, taptreeRoot } = createKeyPathP2trMusig2(inputWalletKeys.publicKeys);
-
-      assert.ok(!input.tapLeafScript);
-      assert.ok(input.tapInternalKey);
-      assert.ok(input.tapMerkleRoot);
-      assert.ok(input.tapInternalKey.equals(internalPubkey));
-      assert.ok(input.tapMerkleRoot.equals(taptreeRoot));
-    } else {
-      assert.ok(input.tapLeafScript);
-      assert.ok(!input.tapInternalKey);
-      assert.ok(!input.tapMerkleRoot);
-      leafHashesCount = 1;
-    }
-    input.tapBip32Derivation?.forEach((bv) => {
-      assert.strictEqual(bv.leafHashes.length, leafHashesCount);
-    });
-  }
-
-  function validatePsbtP2trMusig2Output(psbt: UtxoPsbt<UtxoTransaction<bigint>>, index: number) {
-    const outputWalletKeys = rootWalletKeys.deriveForChainAndIndex(getInternalChainCode(outputType), CHANGE_INDEX);
-    const payment = createPaymentP2trMusig2(outputWalletKeys.publicKeys);
-    const output = psbt.data.outputs[index];
-    assert.ok(!!payment.internalPubkey);
-    assert.ok(!!output.tapInternalKey);
-    assert.ok(output.tapInternalKey.equals(payment.internalPubkey));
-    assert.strictEqual(output.tapBip32Derivation?.length, 3);
-    output.tapBip32Derivation?.forEach((bv) => {
-      const leafHashesCount = bv.pubkey.equals(toXOnlyPublicKey(outputWalletKeys.backup.publicKey)) ? 2 : 1;
-      assert.strictEqual(bv.leafHashes.length, leafHashesCount);
-    });
-  }
-
-  function validateNoncesKeyVals(
-    psbt: UtxoPsbt<UtxoTransaction<bigint>>,
-    index: number,
-    unspent: WalletUnspent<bigint>
-  ) {
-    const keyVals = psbt.getProprietaryKeyVals(index);
-    const inputWalletKeys = rootWalletKeys.deriveForChainAndIndex(unspent.chain, unspent.index);
-    const { outputPubkey } = createKeyPathP2trMusig2(inputWalletKeys.publicKeys);
-    const derivedParticipantPubKeys = [inputWalletKeys.user.publicKey, inputWalletKeys.bitgo.publicKey];
-
-    const noncesKeyVals = keyVals.filter((kv) => kv.key.subtype === ProprietaryKeySubtype.MUSIG2_PUB_NONCE);
-    assert.strictEqual(noncesKeyVals.length, 2);
-
-    const nonceKeydata = derivedParticipantPubKeys.map((p) => {
-      const keydata = Buffer.alloc(65);
-      p.copy(keydata);
-      outputPubkey.copy(keydata, 33);
-      return keydata;
-    });
-
-    noncesKeyVals.forEach((kv) => {
-      assert.strictEqual(kv.key.identifier, PSBT_PROPRIETARY_IDENTIFIER);
-      assert.strictEqual(kv.value.length, 66);
-      assert.strictEqual(nonceKeydata.filter((kd) => kd.equals(kv.key.keydata)).length, 1);
-    });
-  }
-
-  function validateParticipantsKeyVals(
-    psbt: UtxoPsbt<UtxoTransaction<bigint>>,
-    index: number,
-    unspent: WalletUnspent<bigint>
-  ) {
-    const keyVals = psbt.getProprietaryKeyVals(index);
-    const walletKeys = rootWalletKeys.deriveForChainAndIndex(unspent.chain, unspent.index);
-    const { internalPubkey, outputPubkey } = createKeyPathP2trMusig2(walletKeys.publicKeys);
-    const participantPubKeys = [walletKeys.user.publicKey, walletKeys.bitgo.publicKey];
-
-    const participantsKeyVals = keyVals.filter(
-      (kv) => kv.key.subtype === ProprietaryKeySubtype.MUSIG2_PARTICIPANT_PUB_KEYS
-    );
-    assert.strictEqual(participantsKeyVals.length, 1);
-
-    const kv = participantsKeyVals[0];
-    assert.strictEqual(kv.key.identifier, PSBT_PROPRIETARY_IDENTIFIER);
-    assert.ok(Buffer.concat([outputPubkey, internalPubkey]).equals(kv.key.keydata));
-    const valueMatch = [Buffer.concat(participantPubKeys), Buffer.concat(participantPubKeys.reverse())].some((pks) => {
-      return pks.equals(kv.value);
-    });
-    assert.ok(valueMatch);
-  }
 
   describe('p2trMusig2 key path', function () {
     it(`psbt creation and nonce generation success`, function () {
